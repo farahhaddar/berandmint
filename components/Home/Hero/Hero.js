ee7de211746213cfeb49e763352595d40349b967
Ali Hazimeh <ahazimeh@globalistic.tech>
2022-05-29 16:30:20 +0300

    navbar

---
+++ components/Home/Hero/Hero.js
@@ -0,0 +1,11 @@
+const Hero = () => {
+  return (
+    <>
+      <p>
+        Brands are just like People. They have a job, a personality, a style, &
+        most importantly, Brands have dreams.
+      </p>
+    </>
+  );
+};
+export default Hero;
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,32 +1,55 @@
-import Link from 'next/link'
-import Image from "next/image"
+import Link from "next/link";
+import Image from "next/image";
+import classes from "./Navbar.module.css";
+import { useState } from "react";
 
 const Navbar = () => {
-     return ( 
-          <nav>
-               <div className="logo">
-                    <Link href='/'>
-                    <a>
-                         <Image src="/me.jpg" alt="logo" width={128} height={77} />
-                    </a>
-                    </Link>
-               </div>
-               <div class='navLinks'>
-                    <Link href='/work'><a>Work</a></Link>
-                    <Link href='/offering'><a> Offering</a></Link>
-                    <Link href='/clients'><a>Clients</a></Link>
-               </div>
-               <div className="navzcta">
-                    <Link href=''>
-                         <a>
-                              Lets Talk
-                         </a>
-                    </Link>
-               
-               </div>
-              
-          </nav>
-      );
-}
- 
+  const [showNav, setShowNav] = useState(false);
+  return (
+    <nav className={classes.navbar}>
+      <div className={classes.logo}>
+        <Link href="/">
+          <a>
+            <Image src="/images/logo.svg" alt="logo" width={163} height={48} />
+          </a>
+        </Link>
+      </div>
+      <div class={classes.navLinks}>
+        <Link href="/work">
+          <a>Work</a>
+        </Link>
+        <Link href="/offering">
+          <a> Offering</a>
+        </Link>
+        <Link href="/clients">
+          <a>Clients</a>
+        </Link>
+      </div>
+      <div className={classes.navzcta}>
+        <Link href="">
+          <a>Lets Talk</a>
+        </Link>
+      </div>
+      <div
+        onClick={() => {
+          setShowNav((prevState) => {
+            return !prevState;
+          });
+        }}
+        className={classes.mobileNav}
+      >
+        <Image
+          src="/images/sidenav-toggle.svg"
+          alt="logo"
+          width={16}
+          height={14}
+        />
+      </div>
+      <div
+        style={{ position: "fixed", backgroundColor: "white", width: "100%" }}
+      ></div>
+    </nav>
+  );
+};
+
 export default Navbar;
