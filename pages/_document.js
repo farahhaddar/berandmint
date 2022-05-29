048f8084eb4a3abd0e7b3fd4302cf54d7b561683
Ali Hazimeh <ahazimeh@globalistic.tech>
2022-05-29 19:28:50 +0300

    slider

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,16 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <link
+            rel="stylesheet"
+            type="text/css"
+            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+          />
+          <link
+            rel="stylesheet"
+            type="text/css"
+            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+          />
         </body>
       </Html>
     );
