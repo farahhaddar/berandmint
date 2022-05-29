2b94247bf150c930d00c5543a72c040b1d9deedb
farahhaddar <farah.haddar99@gmail.com>
2022-05-29 22:38:47 +0300

    hero

--- components/Home/Hero/Hero.js
+++ components/Home/Hero/Hero.js
@@ -1,11 +1,21 @@
+import Link from "next/link";
+import Image from "next/image";
+import styles from "../../../styles/Home.module.css";
+
 const Hero = () => {
   return (
-    <>
+    <div className='warper blackbg'>
+    <div className="container">
+    <div className={styles.heroSection}>  
       <p>
         Brands are just like People. They have a job, a personality, a style, &
         most importantly, Brands have dreams.
       </p>
-    </>
+      </div>
+      </div>
+    </div>
+
+
   );
 };
 export default Hero;
