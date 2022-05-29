617ef16002ff56a2e5679606b72b6bccc4a2ccdd
farahhaddar <farah.haddar99@gmail.com>
2022-05-29 22:17:09 +0300

    responsive header and footer

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,6 +6,7 @@ import { useRouter } from "next/router";
 import { NavLink } from "./NavLink";
 
 const Navbar = () => {
+
   const [showNav, setShowNav] = useState(false);
   const toggleNav = () => {
     if (showNav) document.body.style.overflow = "auto";
@@ -15,10 +16,12 @@ const Navbar = () => {
       return !prevState;
     });
   };
+
   return (
     <nav>
       <div className="container">
         <div className={styles.navbar}>
+          {/* logo */}
           <div className={styles.logo}>
             <Link href="/">
               <a>
@@ -31,9 +34,10 @@ const Navbar = () => {
               </a>
             </Link>
           </div>
-          <div class={styles.navLinks}>
+          {/* links */}
+          <div className={styles.navLinks}>
             <Link href="/work">
-              <a>Work</a>
+              <a>WORK</a>
             </Link>
             <Link href="/offering">
               <a> Offering</a>
@@ -42,11 +46,13 @@ const Navbar = () => {
               <a>Clients</a>
             </Link>
           </div>
+          {/* btn */}
           <div className={styles.navzcta}>
             <Link href="">
               <a>Lets Talk</a>
             </Link>
           </div>
+          {/* toggle btn*/}
           <div className={styles.navToggle} onClick={toggleNav}>
             <Image
               src="/images/sidenav-toggle.svg"
@@ -55,6 +61,8 @@ const Navbar = () => {
               height={14}
             />
           </div>
+
+          {/* mobile nav */}
           {showNav && (
             <div className={styles.mobileNav}>
               <div className={styles.closeContainer}>
@@ -67,16 +75,20 @@ const Navbar = () => {
                 />
               </div>
               <div className={styles.sideNav}>
+                <NavLink onClick={toggleNav} href="/">
+                  <a className={styles.greyText}>HOME</a>
+                </NavLink>
+
                 <NavLink onClick={toggleNav} href="/work">
                   <a className={styles.greyText}>WORK</a>
                 </NavLink>
-                <NavLink onClick={toggleNav} href="/work">
-                  <a className={styles.greyText}>OFFERING1</a>
+                <NavLink onClick={toggleNav} href="/offering">
+                  <a className={styles.greyText}>OFFERING</a>
                 </NavLink>
-                <NavLink onClick={toggleNav} href="/work">
+                <NavLink onClick={toggleNav} href="/clients">
                   <a className={styles.greyText}>CLIENT</a>
                 </NavLink>
-                <NavLink onClick={toggleNav} href="/work">
+                <NavLink onClick={toggleNav} href="/#talk">
                   <a className={styles.greyText}>LETS TALK</a>
                 </NavLink>
               </div>
@@ -88,16 +100,16 @@ const Navbar = () => {
                   <Image
                     src="/images/facebook.svg"
                     alt="logo"
-                    width={15}
-                    height={30}
+                    width={40}
+                    height={40}
                   />
                 </a>
                 <a href="https://www.instagram.com/brandmint/" target="_blank">
                   <Image
                     src="/images/instagram.svg"
                     alt="logo"
-                    width={29}
-                    height={29}
+                    width={40}
+                    height={40}
                   />
                 </a>
                 <a
@@ -107,11 +119,35 @@ const Navbar = () => {
                   <Image
                     src="/images/linkedIn.svg"
                     alt="logo"
-                    width={29}
-                    height={29}
+                    width={40}
+                    height={40}
                   />
                 </a>
               </div>
+              <div className={styles.navAddress}>
+                <div className={styles.contactItem}>
+                  <div className={styles.icon}>
+                    <svg id="ic_mail" xmlns="http://www.w3.org/2000/svg" width="15.719" height="10.286" viewBox="0 0 15.719 10.286">
+                      <path id="Fill_1" data-name="Fill 1" d="M14.6,10.286H1.123A1.1,1.1,0,0,1,0,9.2V1.083A1.1,1.1,0,0,1,1.123,0H14.6a1.1,1.1,0,0,1,1.123,1.083V9.2A1.1,1.1,0,0,1,14.6,10.286ZM2.246,1.624a.57.57,0,0,0-.439.2.529.529,0,0,0,.087.761L7.508,6.926a.583.583,0,0,0,.7,0l5.614-4.339a.532.532,0,0,0,.207-.365.526.526,0,0,0-.12-.4.577.577,0,0,0-.789-.084L7.86,5.81,2.6,1.742A.567.567,0,0,0,2.246,1.624Z" fill="#00b180" />
+                    </svg>
+                  </div>
+                  <div>
+                    <a className={styles.textItem} href="mailto:info@brandmint.com">info@brandmint.com</a>
+                  </div>
+                </div>
+                <div className={styles.contactItem}>
+                  <div className={styles.icon}>
+                    <svg id="ic_phone" xmlns="http://www.w3.org/2000/svg" width="15.677" height="15.677" viewBox="0 0 15.677 15.677">
+                      <path id="Fill_1" data-name="Fill 1" d="M7.072,8.6C4.341,5.874,4.731,5.484,4.731,5.484L5.9,4.313a1.1,1.1,0,0,0,0-1.56L3.474.325A1.1,1.1,0,0,0,1.968.273,5.744,5.744,0,0,0,.049,3.143c-.39,1.95,1.561,4.681,4.681,7.8s5.852,5.071,7.8,4.681a5.744,5.744,0,0,0,2.87-1.919,1.1,1.1,0,0,0-.052-1.506L12.923,9.775a1.1,1.1,0,0,0-1.56,0l-1.17,1.171s-.39.39-3.121-2.341" fill="#00b180" />
+                    </svg>
+                  </div>
+                  <div>
+                    <a className={styles.textItem} href="tel:+961 1 748 204">+961 1 748 204</a>
+                    <a className={styles.textItem} href="tel:+961 1 748 205">+961 1 748 205</a>
+                  </div>
+                </div>
+              </div>
+
             </div>
           )}
         </div>
