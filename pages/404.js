65a741404dd94574d5511875390a9a6ab243c77e
farahhaddar <farah.haddar99@gmail.com>
2022-06-08 17:49:01 +0300

    404 page

--- pages/404.js
+++ pages/404.js
@@ -6,17 +6,21 @@ import { useRouter } from 'next/router'
           const router = useRouter()
 
           useEffect(() => {
-               setTimeout(() => {
-                    // router.go(-1)
-                    // router.go(1)
-                    router.push('/')
-               }, 5000)
+               // setTimeout(() => {
+               //      // router.go(-1)
+               //      // router.go(1)
+               //      router.push('/')
+               // }, 5000)
           }, [])
      return ( 
-          <main className='not-found'>
-           <h1>oopssssss...</h1>
-           <h2>the page cannot be found</h2>
-           <p>Go back to the home page <Link href='/'><a>Home</a></Link></p>
+          <main>
+               <div className='warper whitebg not-found'>
+                    <div className="container">
+                         <img src="/images/404.png" alt="404 Image" />
+                         <h1>Whoops…this page is not available</h1>
+                         <div className='link'> <Link href='/'><a>Go Back</a></Link></div>
+                    </div>
+               </div>
           </main>
       );
 }
