d5596056043db9e5b272dc8d083fe37ef9ff8d4d
farahhaddar <farah.haddar99@gmail.com>
2022-06-09 10:38:55 +0300

    Desktop changes

--- components/Clinets/Clintes.js
+++ components/Clinets/Clintes.js
@@ -10,7 +10,7 @@ const ClinetsMain = () => {
                <div className="container">
                     
                          <div className={styles.sectionTitle}>
-                              OUR CLIENTS
+                              Our Clinets
                          </div>
                          <div className={styles.secDescLight}>We're proud of helping the best companies deliver positive emotions to their customers. </div>
                          
